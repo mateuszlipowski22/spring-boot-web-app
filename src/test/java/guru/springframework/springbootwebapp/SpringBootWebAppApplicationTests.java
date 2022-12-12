42cb8acf4884306803ad08cb2fae2401d642f9fe
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-12 20:58:35 +0100

    Initial commit

---
+++ src/test/java/guru/springframework/springbootwebapp/SpringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springbootwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
